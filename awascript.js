0b403550ca5154d727fadb42d6bfe4d13cb395af
x632 <andreas@poema.se>
2019-12-24 11:10:26 +0100

    Ema och Ma tillagt

--- awascript.js
+++ awascript.js
@@ -1,44 +1,87 @@
 //setInterval('getISS()', 3000)
-    var arr,aktiv=false,flyttKorTimer,flyttaKordTimer,rikt=0,riktning=0, bredd=4;
+    var arr,aktiv=false,flyttKorTimer,flyttaKordTimer,rikt=0,riktning=0, bredd=7;
+    //https://www.alphavantage.co/query?function=EMA&symbol=MSFT&interval=weekly&time_period=10&series_type=open&apikey=PI94RGOINPZE8JOZ'
     //const api_url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=STO:LUC&interval=5min&outputsize=compact&apikey=PI94RGOINPZE8JOZ'
     //const api_url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol='+symbol+'&outputsize=compact&apikey=PI94RGOINPZE8JOZ'
-    var skrivUt = [];  var open=[],high =[],low=[],close=[], aktivArrayStorlek = 130;
+    var skrivUt = [],open=[],high =[],low=[],close=[], aktivArrayStorlek = 130,ma=[],ema=[],visaEMA=true,visaMA=true;
     var riktning=0;rikt=0;
+    var timeSeries;
     function getAktie(data){
-       
+        const func = selFunction.value;
+        console.log(func);
+        if (func=="TIME_SERIES_DAILY")
+        {timeSeries="Time Series (Daily)"}
+        if (func=="TIME_SERIES_WEEKLY")
+        {timeSeries="Weekly Time Series"}
+        if (func=="TIME_SERIES_INTRADAY")
+        {timeSeries="Time Series (60min)"}
+        if (func=="TIME_SERIES_MONTHLY")
+        {timeSeries="Monthly Time Series"}
         rikt=0;riktning=0; 
         var data2=JSON.parse(data);
         console.log(data2);
         var aktien= data2["Meta Data"]["2. Symbol"];
         document.getElementById("aktien").textContent=aktien;
-        arr = Object.entries(data2["Time Series (Daily)"]);
+        arr=Object.entries(data2[timeSeries]);
         for (i = 0;i < arr.length; i++){
             var _tid = (arr[i][0]);
             open[i] = parseFloat(arr[i][1]["1. open"]);
             high[i] = parseFloat(arr[i][1]["2. high"]);    
             low[i] = parseFloat(arr[i][1]["3. low"]);
             close[i] = parseFloat(arr[i][1]["4. close"]); 
-
-            //skrivUt [i]="Tid "+tid+" Open: "+open+" High "+high+" Low "+low+" Close "+close+" ";
-            
-            }
+        }
         ritaUtCandleSticks(0)
+    }
+    function aktArrStorl(){
+        aktivArrayStorlek = parseInt(document.getElementById("jusstorl").value);
+            ritaUtCandleSticks(riktning);
+        }
+    function maInput(maAnta){
+        if (maAnta!='remove') {
+            for (i = 0; i < close.length; i++){ //loopar alla cs
+            utrva=0.0;
+            for (j = i; j < i+maAnta ; j++) {  //loopar antalMA på alla cs
+            utrva += close[j];
+            }
+            ma[i]=utrva/maAnta;         
+            }  
+            visaMA=true;
+        }
+        else {
+             visaMA=false;
+                
+        }
+        ritaUtCandleSticks(riktning)
+    }   
+    function emaInput(emaAnta){
+        if (emaAnta != 'remove') {
+            var k = 2.0/(emaAnta + 1.0);b=close.length-1;
+            console.log("length: "+b);
+            ema [2000]= close[2000];
+            for (var i = 1999; i >0; i--) {
+              ema[i]=(close[i] * k + ema[i + 1.0] * (1.0 - k));
+            }
+            visaEMA=true;    
+        }
+        else{visaEMA=false;}
+        ritaUtCandleSticks(riktning) 
     }
     function channel(){
         riktning = parseInt(document.getElementById("koordinat").value);
         if (riktning>=close.length) {
             riktning=close.length-aktivArrayStorlek;
-            console.log(close.length);
+            console.log("Close length"+close.length);
         } 
+        
         console.log(riktning);
         ritaUtCandleSticks(riktning);
-    }
+        }
     
     function sattBredd(){
-    bredd = parseInt(document.getElementById("jusbredd").value);
-    console.log(bredd);
-    ritaUtCandleSticks(0);
-    }
+        bredd = parseInt(document.getElementById("jusbredd").value);
+        console.log(bredd);
+        ritaUtCandleSticks(riktning);
+        }
     function raknaUtRange(rikt){
         var aktivaArraynHigh = [],aktivaArraynLow = [],minsta,range = [],hogsta;
         minsta =high[rikt];
@@ -56,34 +99,14 @@
         return range;
     }    
    
-    /* function sattTimerRight(aktiv){
-        if (aktiv==true) {
-            console.log("Varit här!");
-            sattTimerLeft(false);
-            flyttaKordTimer=setInterval('ritaUtCandleSticks(-1)', 100);
-            }
-                else if (aktiv==false) { 
-                clearInterval(flyttaKordTimer)
-                }
-        } */
-    /* 
-        function sattTimerLeft(aktiv){
-            if (aktiv==true) {
-                console.log("Varit här!")
-                sattTimerRight(false);
-                flyttKorTimer=setInterval('ritaUtCandleSticks(1)', 100);
-                }
-            else if (aktiv == false){
-                    clearInterval(flyttKorTimer)
-                } 
-        }*/
+  
     function ritaUtCandleSticks(riktning){
         if(riktning<0){riktning=0;}
         console.log("variabeln som har tagits emot: "+riktning);
         range=raknaUtRange(riktning);
         var canvas = document.getElementById("myCanvas");
         var ctx = canvas.getContext("2d");
-        var aktivArrayStorlek = 130;//hur många csticks
+        //var aktivArrayStorlek = 130;//hur många csticks
         var ch=460,avstand=9,hojd,utr; //utr = variabeln som spegelvänder aktien 
         var hojd = (ch/range[0]);//range[0] = skillnaden mellan högsta och lägsta - "rangen"
         //range[3] = den procentuella skillnaden mellan range och högsta
@@ -124,12 +147,21 @@
         ctx.fillStyle = '#000095';
         ctx.fillText(pristext, 40, ch-(close[0]*hojd)+14);
         ctx.stroke();
-    
         ctx.setLineDash([]);//ta bort punkter från linjeritningen
     //rita ut candlesticks*********************************************************
     for (i = aktivArrayStorlek; i > 0 ; i-- ){ //från aktivt arrayområdes slut till början			
         utr=(aktivArrayStorlek-i)+riktning;
-    
+        
+            //rita ut MA
+            if (visaMA){
+            ctx.fillStyle = '#0000cc';
+            ctx.fillRect(i*avstand+bredd+(bredd/2),ch-(ma[utr]*hojd),2,2);
+            }
+            //rita ut EMA
+            if (visaEMA){
+            ctx.fillStyle = '#CC0000';
+            ctx.fillRect(i*avstand+bredd+(bredd/2),ch-(ema[utr]*hojd),2,2);
+            }
     if (open[utr]>close[utr]) 	{				//BEARcandle
             //Rita kropp           
             ctx.lineWidth=1;
@@ -192,10 +224,15 @@
         ctx.strokeStyle ="#000000";
         ctx.moveTo(i*avstand+bredd+(bredd/2),ch-open[utr]*hojd);
         ctx.lineTo(i*avstand+bredd+(bredd/2),ch-low[utr]*hojd);
-        ctx.stroke();				                               
+        ctx.stroke();	
+       			                               
     }
     } 
 }
+
+//https://www.alphavantage.co/query?function=EMA&symbol=MSFT&interval=weekly&time_period=10&series_type=open&apikey=PI94RGOINPZE8JOZ'
+//https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol='+symbol+'&outputsize=compact&apikey=PI94RGOINPZE8JOZ'
+var timeSeries;
 function getAlphaVantagedata() {
     const func = selFunction.value; //'function is a reserved word
     const size = selSize.value;
